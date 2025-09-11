d55fdc31e6ce323bc16e8465e9de06fea0856af6
nanu <ranusate19@gmail.com>
2025-09-11 13:05:29 +0800

    `Removed commented out style block in _variables.blade.php`

--- resources/views/partials/_variables.blade.php
+++ resources/views/partials/_variables.blade.php
@@ -1,7 +1,7 @@
-<style>
+{{-- <style>
     :root {
         --color-silet-primary: {{ $theme->primary_color }};
         --color-silet-secondary: {{ $theme->secondary_color }};
         --color-silet-menu-bg: {{ $theme->menu_background }};
     }
-</style>
+</style> --}}
