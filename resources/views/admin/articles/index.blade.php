f3e77e80e52b09a5c709475037f102d1cf018002
nanu <ranusate19@gmail.com>
2025-09-19 12:50:07 +0800

    update

--- resources/views/admin/articles/index.blade.php
+++ resources/views/admin/articles/index.blade.php
@@ -71,26 +71,14 @@
                                     <div
                                         class="flex flex-wrap items-center justify-between text-sm font-medium pt-3 border-t dark:border-gray-600">
                                         <a href="{{ route('articles.show', $article->slug) }}"
-                                            class="flex items-center text-indigo-500 hover:text-indigo-700 dark:text-indigo-400 dark:hover:text-indigo-200 transition"
+                                            class="bg-green-600 hover:bg-green-700 text-white px-3 py-1 rounded transition"
                                             title="Lihat Berita">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1"
-                                                viewBox="0 0 20 20" fill="currentColor">
-                                                <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                                                <path fill-rule="evenodd"
-                                                    d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                                                    clip-rule="evenodd" />
-                                            </svg>
                                             Lihat
                                         </a>
                                         <div class="flex items-center space-x-3">
                                             <a href="{{ route('admin.articles.edit', $article->slug) }}"
-                                                class="flex items-center text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-200 transition"
+                                                class="bg-blue-600 hover:bg-blue-700 text-white px-3 py-1 rounded transition"
                                                 title="Edit Berita">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1"
-                                                    viewBox="0 0 20 20" fill="currentColor">
-                                                    <path
-                                                        d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-                                                </svg>
                                                 Edit
                                             </a>
                                             <form action="{{ route('admin.articles.destroy', $article->slug) }}"
@@ -99,14 +87,8 @@
                                                 @csrf
                                                 @method('DELETE')
                                                 <button type="submit"
-                                                    class="flex items-center text-red-600 hover:text-red-800 dark:text-red-400 dark:hover:text-red-200 transition"
-                                                    title="Hapus Berita">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1"
-                                                        viewBox="0 0 20 20" fill="currentColor">
-                                                        <path fill-rule="evenodd"
-                                                            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                                            clip-rule="evenodd" />
-                                                    </svg>
+                                                    class="bg-red-600 hover:bg-red-700 text-white px-3 py-1 rounded transition"
+                                                    title="Hapus Berita"> Hapus
                                                 </button>
                                             </form>
                                         </div>
