025cdd530c5ca1b883b1bce286d13c0413c41a5e
nanu <ranusate19@gmail.com>
2025-09-19 15:48:08 +0800

    update

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -111,9 +111,7 @@
                             @if ($article->hasMedia('images'))
                                 <picture>
                                     {{-- Sumber untuk gambar WebP responsif --}}
-                                    <source srcset="{{ $article->getFirstMedia('images')->getSrcset('webp') }}"
-                                        type="image/webp">
-
+                                    <source srcset="{{ $article->getFirstMedia('images')->getSrcset('webp') }}">
                                     {{-- Gambar fallback (asli) dengan srcset --}}
                                     <img srcset="{{ $article->getFirstMedia('images')->getSrcset() }}"
                                         src="{{ $article->getFirstMediaUrl('images') }}" alt="{{ $article->title }}"
