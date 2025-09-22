6f2109ebd67ba1f3a53888ed58b661e741ce4c9e
nanu <ranusate19@gmail.com>
2025-09-22 17:18:29 +0800

    update

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -70,7 +70,14 @@
                         <div class="flex items-center space-x-6 text-gray-600 dark:text-gray-400 text-sm">
                             <!-- Views -->
                             <div class="flex items-center space-x-1">
-                                <i class="fas fa-eye"></i>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                </svg>
+
                                 <span>{{ number_format($article->views) }}</span>
                             </div>
 
@@ -113,7 +120,6 @@
                                     <path stroke-linecap="round" stroke-linejoin="round"
                                         d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
                                 </svg>
-
                                 <span>{{ $article->comments_count ?? 0 }}</span>
                             </div>
                         </div>
@@ -122,7 +128,12 @@
                             <!-- Tombol Share -->
                             <button @click="open = !open"
                                 class="flex items-center space-x-1 text-gray-600 dark:text-gray-400 hover:text-green-600 transition p-1 rounded">
-                                <i class="fas fa-share"></i>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z" />
+                                </svg>
+
                                 <span>Bagikan</span>
                             </button>
 
@@ -158,7 +169,8 @@
                         class="hidden fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
                         <div class="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg w-80 text-center space-y-4">
                             <h3 class="font-bold text-lg text-gray-800 dark:text-white">Silakan login</h3>
-                            <p class="text-gray-600 dark:text-gray-300 text-sm">Anda harus login untuk menyukai artikel ini.
+                            <p class="text-gray-600 dark:text-gray-300 text-sm">Anda harus login untuk menyukai artikel
+                                ini.
                             </p>
                             <a href="{{ route('login') }}"
                                 class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition">Login</a>
