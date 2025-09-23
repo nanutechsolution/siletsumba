1bdca4fd26ad77a78d36fa9d22fb97a89cc4c1b5
nanu <ranusate19@gmail.com>
2025-09-23 12:30:13 +0800

    update

--- resources/views/partials/frontend-navbar-darkmode.blade.php
+++ resources/views/partials/frontend-navbar-darkmode.blade.php
@@ -139,6 +139,9 @@
                      </x-slot>
                      <x-slot name="content">
                          <x-dropdown-link :href="route('admin.dashboard')">Dashboard</x-dropdown-link>
+                         @can('buat_artikel')
+                             <x-dropdown-link :href="route('admin.articles.create')">+ Berita</x-dropdown-link>
+                         @endcan
                          <x-dropdown-link :href="route('profile.edit')">Profil</x-dropdown-link>
                          <form method="POST" action="{{ route('logout') }}">
                              @csrf
