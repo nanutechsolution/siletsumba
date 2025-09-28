1e404363971eef163f0734a2aafc9dd1e1e459fd
nanu <ranusate19@gmail.com>
2025-09-28 22:51:20 +0800

    update

--- resources/views/partials/frontend-footer.blade.php
+++ resources/views/partials/frontend-footer.blade.php
@@ -10,18 +10,20 @@
                     {{ $settings['site_description']->value ?? 'Media berita terkini dan terpercaya dari Sumba.' }}
                 </p>
                 <div class="flex space-x-3">
-                    @foreach (['facebook' => 'fab fa-facebook', 'twitter' => 'fab fa-twitter', 'instagram' => 'fab fa-instagram', 'youtube' => 'fab fa-youtube'] as $platform => $icon)
-                        @php
-                            $urlKey = "social_{$platform}_url";
-                            $label = ucfirst($platform);
-                        @endphp
-                        @if (!empty($settings[$urlKey]->value))
-                            <a href="{{ $settings[$urlKey]->value }}" target="_blank" rel="noopener noreferrer"
-                                class="text-gray-400 hover:text-white transition"
-                                aria-label="Ikuti kami di {{ $label }}">
-                                <i class="{{ $icon }} text-lg"></i>
-                            </a>
-                        @endif
+                    @foreach (['facebook' => 'M18 2h-3a6 6 0 0 0-6 6v3H6v4h3v8h4v-8h3l1-4h-4V8a1 1 0 0 1 1-1h3V2z',
+                    'twitter' => 'M23 3a10.9 10.9 0 0 1-3.14 1.53A4.48 4.48 0 0 0 22.4.36a9.05 9.05 0 0 1-2.88 1.1 4.52 4.52 0 0 0-7.72 4.12A12.85 12.85 0 0 1 1.64 2.16a4.52 4.52 0 0 0 1.4 6.05A4.41 4.41 0 0 1 .96 7.7v.05a4.52 4.52 0 0 0 3.63 4.42 4.52 4.52 0 0 1-2.04.08 4.52 4.52 0 0 0 4.22 3.13A9.07 9.07 0 0 1 0 19.54 12.81 12.81 0 0 0 6.92 21c8.32 0 12.87-6.9 12.87-12.87 0-.2 0-.42-.01-.63A9.2 9.2 0 0 0 24 4.56a9.3 9.3 0 0 1-2.54.7z',
+                    'instagram' => 'M7 2C4.24 2 2 4.24 2 7v10c0 2.76 2.24 5 5 5h10c2.76 0 5-2.24 5-5V7c0-2.76-2.24-5-5-5H7zm0 2h10c1.66 0 3 1.34 3 3v10c0 1.66-1.34 3-3 3H7c-1.66 0-3-1.34-3-3V7c0-1.66 1.34-3 3-3zm8 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2z'] as $platform => $svgPath)
+                    @php
+                    $urlKey = "social_{$platform}_url";
+                    $label = ucfirst($platform);
+                    @endphp
+                    @if (!empty($settings[$urlKey]->value))
+                    <a href="{{ $settings[$urlKey]->value }}" target="_blank" rel="noopener noreferrer" class="text-gray-400 hover:text-white transition" aria-label="Ikuti kami di {{ $label }}">
+                        <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24">
+                            <path d="{{ $svgPath }}"></path>
+                        </svg>
+                    </a>
+                    @endif
                     @endforeach
                 </div>
             </div>
@@ -31,12 +33,11 @@
                 <h4 class="font-bold text-lg mb-4">{{ __('KATEGORI UTAMA') }}</h4>
                 <ul class="space-y-2">
                     @foreach ($footerCategories as $category)
-                        <li>
-                            <a href="{{ route('articles.category', $category->slug) }}"
-                                class="text-gray-300 dark:text-gray-400 hover:text-silet-red transition text-sm dark:hover:text-silet-red">
-                                {{ $category->name }}
-                            </a>
-                        </li>
+                    <li>
+                        <a href="{{ route('articles.category', $category->slug) }}" class="text-gray-300 dark:text-gray-400 hover:text-silet-red transition text-sm dark:hover:text-silet-red">
+                            {{ $category->name }}
+                        </a>
+                    </li>
                     @endforeach
                 </ul>
             </div>
@@ -46,12 +47,11 @@
                 <h4 class="font-bold text-lg mb-4">{{ __('TENTANG KAMI') }}</h4>
                 <ul class="space-y-2 text-sm">
                     @foreach ($footerPages as $page)
-                        <li>
-                            <a href="{{ route('page.show', $page->slug) }}"
-                                class="text-gray-300 dark:text-gray-400 hover:text-silet-red transition dark:hover:text-silet-red">
-                                {{ $page->title }}
-                            </a>
-                        </li>
+                    <li>
+                        <a href="{{ route('page.show', $page->slug) }}" class="text-gray-300 dark:text-gray-400 hover:text-silet-red transition dark:hover:text-silet-red">
+                            {{ $page->title }}
+                        </a>
+                    </li>
                     @endforeach
                 </ul>
             </div>
