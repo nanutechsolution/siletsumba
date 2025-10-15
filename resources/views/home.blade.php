f0c11280fbc4424f876999b5836f4037034d1c05
nanu <ranusate19@gmail.com>
2025-10-15 19:36:39 +0800

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -76,7 +76,7 @@
 
                 {{-- Konten berita --}}
                 <div class="p-3 md:p-4 flex flex-col justify-start min-h-[180px] bg-white dark:bg-gray-800 rounded shadow dark:shadow-lg transition-colors duration-300">
-                    <div class="font-bold text-base md:text-lg text-gray-800 dark:text-white line-clamp-2 leading-6 mb-1">
+                    <div class="font-bold text-base md:text-lg text-gray-800 dark:text-white line-clamp-4 leading-6 mb-1">
                         {{ $article->title }}
                     </div>
                     <p class="text-gray-600 dark:text-gray-300 text-sm line-clamp-3 leading-5 mb-2">
@@ -101,7 +101,6 @@
 
                     </div>
                 </div>
-
             </a>
             {{-- 🔸 Iklan di tengah daftar berita --}}
             @if ($loop->iteration == 3 || $loop->iteration == 6)
